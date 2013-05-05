9e64b2a35c7621a84437cc59e35ab33ef5b4f468
zjuerkzhang@gmail.com <zjuerkzhang@gmail.com@09bfffbe-ea3f-11de-9229-05bbbb8527c3>
2013-05-05 08:37:24 +0000

    1. 修改应用有效性验证算法； 2. 将单价作为配置信心，存入配置表；单价数据只在计算器界面可修改； 3. 历史记录中显示配置表中的单价数据，并计算相应总价； 4. 历史记录中，提供重新计算按钮，便于适用新单价。

--- Android/SmartGpsMeasure/src/com/example/smartgpsmeasurer/CalcActivity.java
+++ Android/SmartGpsMeasure/src/com/example/smartgpsmeasurer/CalcActivity.java
@@ -1,7 +1,11 @@
 package com.example.smartgpsmeasurer;
 
+import com.example.smartgpsmeasurer.MainActivity.AreaUnit;
+import com.example.smartgpsmeasurer.MainActivity.DistanceUnit;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +16,7 @@ public class CalcActivity extends Activity {
 	static final String tag = "CalcActivity";
 	
 	private String m_unit_price ="";
+	AreaUnit.AreaUnitType m_area_unit = AreaUnit.AreaUnitType.SQ_METER;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,17 +26,53 @@ public class CalcActivity extends Activity {
         
         Intent intent = getIntent();
         String measurement = intent.getStringExtra(MainActivity.EXTRA_MEASUREMENT);
-        String unit = intent.getStringExtra(MainActivity.EXTRA_UNIT);
+        int unit = intent.getIntExtra(MainActivity.EXTRA_UNIT, 0);
+        m_area_unit = AreaUnit.AreaUnitType.values()[unit];
+        String unit_str = AreaUnit.getAreaUnitText(getApplicationContext(), m_area_unit);
      
         setContentView(R.layout.activity_calc);
         
         TextView tv = (TextView)findViewById(R.id.txt_id_cal_area);
         tv.setText(measurement);
         tv = (TextView)findViewById(R.id.txt_id_cal_area_unit);
-        tv.setText(unit);
+        tv.setText(unit_str);
         tv = (TextView)findViewById(R.id.txt_id_cal_unit_price_unit);
-        tv.setText(this.getString(R.string.txt_static_money_unit)+"/"+unit);
+        tv.setText(this.getString(R.string.txt_static_money_unit)+"/"+unit_str);
     }
+	
+	@Override
+	public void onStart()
+	{
+		MainActivity.myDebugLog(tag, "onStart");
+		super.onStart();
+		
+		SharedPreferences settings = getSharedPreferences(MainActivity.CONFIG_FILE, 0);
+		float unit_price_per_m2 = settings.getFloat(MainActivity.CONFIG_UNIT_PRICE, 0);
+		m_unit_price = String.format("%.2f", unit_price_per_m2/AreaUnit.getAreaConversionRate(m_area_unit));
+		
+		MainActivity.myDebugLog(tag, "m_area_unit: "+m_area_unit.toString());
+		MainActivity.myDebugLog(tag, String.format("unit_price_per_m2: %f", unit_price_per_m2));
+		
+		TextView tv = (TextView)findViewById(R.id.txt_id_cal_unit_price);
+		tv.setText(m_unit_price);
+	}
+	
+	@Override
+	public void onStop()
+	{
+		MainActivity.myDebugLog(tag, "onStop");
+		super.onStop();
+		
+		if(m_unit_price != "")
+		{
+			float unit_price_per_m2 = Float.parseFloat(m_unit_price)*((float)AreaUnit.getAreaConversionRate(m_area_unit));
+			
+			SharedPreferences settings = getSharedPreferences(MainActivity.CONFIG_FILE, 0);
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putFloat(MainActivity.CONFIG_UNIT_PRICE, unit_price_per_m2);
+			editor.commit();
+		}
+	}
 	
 	public void onNumPadClick(View p_view)
 	{
@@ -63,13 +104,13 @@ public class CalcActivity extends Activity {
 		
 		if( m_unit_price!="" )
 		{
-		TextView tv = (TextView)findViewById(R.id.txt_id_cal_area);
-		double area = Double.parseDouble(tv.getText().toString());
-		double unit_price = Double.parseDouble(m_unit_price);
-		
-		double total_price = area*unit_price;
-		tv = (TextView)findViewById(R.id.txt_id_cal_total_price);
-		tv.setText(String.format("%.2f", total_price));
+			TextView tv = (TextView)findViewById(R.id.txt_id_cal_area);
+			double area = Double.parseDouble(tv.getText().toString());
+			double unit_price = Double.parseDouble(m_unit_price);
+
+			double total_price = area*unit_price;
+			tv = (TextView)findViewById(R.id.txt_id_cal_total_price);
+			tv.setText(String.format("%.2f", total_price));
 		}
 	}
 
